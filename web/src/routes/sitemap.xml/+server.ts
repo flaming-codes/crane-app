cfee30f003f4cca8a0c380f6f9f36fc3412b8386
Tom Schönmann <11635736+flaming-codes@users.noreply.github.com>
2022-09-07 23:15:30 +0200

    chore: simplify sitemap (#31)
    Co-authored-by: Tom Schönmann <tom@wild.as>

--- web/src/routes/sitemap.xml/+server.ts
+++ web/src/routes/sitemap.xml/+server.ts
@@ -4,41 +4,41 @@ import { error, type RequestHandler } from '@sveltejs/kit';
 
 export const prerender = true;
 
-export const GET: RequestHandler = async ({ setHeaders }) => {
+export const GET: RequestHandler = async () => {
   const tuples = await sitemapTuples();
 
   if (!tuples) {
     throw error(500, 'Failed to fetch sitemap tuples');
   }
 
-  setHeaders({
-    'Cache-Control': `max-age=${60 * 60 * 24}`
-  });
+  const encodeXML = (source: string) =>
+    source
+      .replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&apos;');
 
   return new Response(
-    `
-        <?xml version="1.0" encoding="UTF-8" ?>
-        <urlset
-          xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
-          xmlns:xhtml="https://www.w3.org/1999/xhtml"
-          xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
-          xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
-          xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
-          xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
-        >
-          ${composeUrlElement({ path: '/', lastmod: '2022-08-20' })}
-          ${composeUrlElement({ path: '/about', lastmod: '2022-08-20' })}
-          ${composeUrlElement({ path: '/how-to', lastmod: '2022-08-20' })}
-          ${tuples
-            .map(([slug, lastmod]) =>
-              composeUrlElement({ path: `/package/${slug}`, lastmod, priority: 1 })
-            )
-            .join('\n')}
-        </urlset>
-      `.trim(),
+    encodeXML(`
+    <?xml version="1.0" encoding="UTF-8" ?>
+    <urlset
+      xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+    >
+      ${composeUrlElement({ path: '/', lastmod: '2022-08-20' })}
+      ${composeUrlElement({ path: '/about', lastmod: '2022-08-20' })}
+      ${composeUrlElement({ path: '/how-to', lastmod: '2022-08-20' })}
+      ${tuples
+        .map(([slug, lastmod]) =>
+          composeUrlElement({ path: `/package/${slug}`, lastmod, priority: 1 })
+        )
+        .join('\n')}
+    </urlset>
+  `).trim(),
     {
       headers: {
-        'Content-Type': 'application/xml'
+        'Content-Type': 'application/xml',
+        'Cache-Control': 'max-age=0, s-maxage=3600'
       }
     }
   );
