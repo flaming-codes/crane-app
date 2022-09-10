ad293fb50739af266a7749f71003026adc49d907
Tom Schönmann <11635736+flaming-codes@users.noreply.github.com>
2022-09-10 16:58:31 +0200

    fix: duplicate jsonld (#43)
    * fix: only generate jsonld on server
    
    * chore: add docs
    
    * fix: invalid import
    
    Co-authored-by: Tom Schönmann <tom@wild.as>

--- web/src/lib/seo/views/BreadcrumbMeta.svelte
+++ web/src/lib/seo/views/BreadcrumbMeta.svelte
@@ -1,18 +1,22 @@
 <script lang="ts">
-  import SvelteSeo from 'svelte-seo';
+  import { browser } from '$app/environment';
+  import { JsonLd } from 'svelte-meta-tags';
 
   export let items: Array<{ name: string; href: string }> = [];
 </script>
 
-<SvelteSeo
-  jsonLd={{
-    '@context': 'https://schema.org',
-    '@type': 'BreadcrumbList',
-    itemListElement: [{ name: 'CRAN/E', href: '' }, ...items].map((item, i) => ({
-      '@type': 'ListItem',
-      position: i + 1,
-      name: item.name,
-      item: 'https://www.cran-e.com' + item.href
-    }))
-  }}
-/>
+<!-- Only run on server (https://github.com/oekazuma/svelte-meta-tags/issues/372) -->
+{#if !browser}
+  <JsonLd
+    schema={{
+      '@context': 'https://schema.org',
+      '@type': 'BreadcrumbList',
+      itemListElement: [{ name: 'CRAN/E', href: '' }, ...items].map((item, i) => ({
+        '@type': 'ListItem',
+        position: i + 1,
+        name: item.name,
+        item: 'https://www.cran-e.com' + item.href
+      }))
+    }}
+  />
+{/if}
--- web/src/lib/seo/views/PersonMeta.svelte
+++ web/src/lib/seo/views/PersonMeta.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
-  import SvelteSeo from 'svelte-seo';
+  import { browser } from '$app/environment';
+  import { JsonLd } from 'svelte-meta-tags';
 
   export let name: string;
 </script>
 
-<SvelteSeo
-  jsonLd={{
-    '@context': 'https://schema.org',
-    '@type': 'Person',
-    name: name,
-    url: 'https://www.cran-e.com/author/' + encodeURIComponent(name)
-  }}
-/>
+<!-- Only run on server (https://github.com/oekazuma/svelte-meta-tags/issues/372) -->
+{#if !browser}
+  <JsonLd
+    schema={{
+      '@context': 'https://schema.org',
+      '@type': 'Person',
+      name: name,
+      url: 'https://www.cran-e.com/author/' + encodeURIComponent(name)
+    }}
+  />
+{/if}
