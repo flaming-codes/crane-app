437bdaddb50fe6227689ba37b53b2b99c6d96d43
Tom Schönmann <tom@wild.as>
2022-09-11 10:23:19 +0200

    chore: update docs

--- web/tests/routes/author.sitemap.test.ts
+++ web/tests/routes/author.sitemap.test.ts
@@ -5,7 +5,7 @@ import { fetchAuthors } from '../utils.ts';
 test('authors sitemap threshold', async () => {
   // Current threshold that defines how many sitemap files to generate.
   // If this test fails, add a new sitemap and increment the threshold.
-  // Increment should be 5k.
+  // Increment should be 10k.
   const threshold = 30_000;
   const record = await fetchAuthors();
   expect(Object.keys(record).length).toBeLessThan(threshold);
--- web/tests/routes/package.sitemap.test.ts
+++ web/tests/routes/package.sitemap.test.ts
@@ -5,7 +5,7 @@ import { fetchOverviewItems } from '../utils.ts';
 test('package sitemap threshold', async () => {
   // Current threshold that defines how many sitemap files to generate.
   // If this test fails, add a new sitemap and increment the threshold.
-  // Increment should be 5k.
+  // Increment should be 10k.
   const threshold = 20_000;
   const items = await fetchOverviewItems();
   expect(items.length).toBeLessThan(threshold);
