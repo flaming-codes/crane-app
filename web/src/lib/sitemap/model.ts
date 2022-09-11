6b215673fe1b40f178409e65b75714a89fdd7f2d
Tom Schönmann <tom@wild.as>
2022-09-11 16:04:14 +0200

    fix: escape xml special chars

--- web/src/lib/sitemap/model.ts
+++ web/src/lib/sitemap/model.ts
@@ -1,3 +1,9 @@
+import { encodeSitemapSymbols } from './parse';
+
+export function getTodayLastmod() {
+  return new Date().toISOString().slice(0, 10);
+}
+
 /**
  * Generate a single <url> element for the sitemap.xml file.
  * @param params
@@ -13,6 +19,18 @@ export function composeUrlElement(params: {
   return `<url><loc>https://www.cran-e.com${path}</loc><lastmod>${lastmod}</lastmod><changefreq>${changefreq}</changefreq><priority>${priority}</priority></url>`.trim();
 }
 
-export function getTodayLastmod() {
-  return new Date().toISOString().slice(0, 10);
+export function composeAuthorUrl(name: string) {
+  return composeUrlElement({
+    path: `/author/${encodeSitemapSymbols(encodeURIComponent(name))}`,
+    lastmod: getTodayLastmod(),
+    priority: 0.8
+  });
+}
+
+export function composePackageUrl([slug, lastmod]: [string, string]) {
+  return composeUrlElement({
+    path: `/package/${encodeSitemapSymbols(slug)}`,
+    lastmod,
+    priority: 1
+  });
 }
