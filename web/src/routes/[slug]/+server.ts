48d0e4d6ca26bc0678a3870404fa409385074604
Tom Schönmann <tom@wild.as>
2023-01-07 21:45:43 +0100

    feat: add statistic sitemap

--- web/src/routes/[slug]/+server.ts
+++ web/src/routes/[slug]/+server.ts
@@ -5,6 +5,7 @@ import {
   mapComposerToDomain,
   mapDomainToSitemapData
 } from '$lib/sitemap/model';
+import { githubTrendRanges } from '$lib/statistics/models/github';
 import { error, type RequestHandler } from '@sveltejs/kit';
 
 /**
@@ -22,7 +23,7 @@ export const GET: RequestHandler = async (props) => {
   const { params } = props;
   const { slug } = params;
 
-  if (slug === 'sitemap-common') {
+  if (slug === 'sitemap-common.xml') {
     const today = getTodayLastmod();
 
     return new Response(
@@ -31,6 +32,33 @@ export const GET: RequestHandler = async (props) => {
           ${composeUrlElement({ path: '', lastmod: today })}
           ${composeUrlElement({ path: '/about', lastmod: '2022-08-20', changefreq: 'monthly' })}
           ${composeUrlElement({ path: '/how-to', lastmod: '2022-08-20', changefreq: 'monthly' })}
+      </urlset>`.trim(),
+      {
+        headers: {
+          'Content-Type': 'application/xml',
+          'Cache-Control': 'public, max-age=3600, s-maxage=3600'
+        }
+      }
+    );
+  }
+
+  if (slug === 'sitemap-statistic.xml') {
+    const today = getTodayLastmod();
+
+    return new Response(
+      `<?xml version="1.0" encoding="UTF-8" ?>
+      <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
+          ${['repos-by-stars', 'users-by-followers'].map((domain) =>
+            githubTrendRanges
+              .map((range) =>
+                composeUrlElement({
+                  path: `/statistic/github/${domain}/${range}`,
+                  lastmod: today,
+                  changefreq: 'daily'
+                })
+              )
+              .join('')
+          )}
       </urlset>`.trim(),
       {
         headers: {
@@ -71,5 +99,7 @@ export const GET: RequestHandler = async (props) => {
     );
   }
 
+  // All static routes (i.e. files) take precedence over dynamic routes,
+  // so we can safely assume that the request is for a dynamic route only.
   throw error(404, 'Not found');
 };
