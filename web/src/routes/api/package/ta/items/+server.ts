b39ad780e2538725671e3c7b36d9671015c57342
Tom Schönmann <tom@wild.as>
2023-01-27 11:38:15 +0100

    fix: missing fetch

--- web/src/routes/api/package/ta/items/+server.ts
+++ web/src/routes/api/package/ta/items/+server.ts
@@ -2,8 +2,8 @@ import { json } from '@sveltejs/kit';
 import { typeAheadTuples } from '$lib/db/model';
 import type { RequestHandler } from '@sveltejs/kit';
 
-export const GET: RequestHandler = async ({ setHeaders }) => {
-  const items = await typeAheadTuples();
+export const GET: RequestHandler = async ({ setHeaders, fetch }) => {
+  const items = await typeAheadTuples(fetch);
 
   setHeaders({
     'Cache-Control': `max-age=${60 * 60 * 4}`
