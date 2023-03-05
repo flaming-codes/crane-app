d2889a37e8140bca3f890b9f3afeb88a1c22c455
Tom Schönmann <tom@wild.as>
2023-03-05 18:41:50 +0100

    chore: revert isr

--- web/src/routes/+layout.ts
+++
@@ -1,7 +0,0 @@
-import type { Config } from '@sveltejs/kit';
-
-export const config: Config = {
-  isr: {
-    expiration: 60 // 1 minute
-  }
-};
--- web/src/routes/[slug]/+layout.ts
+++
@@ -1,7 +0,0 @@
-import type { Config } from '@sveltejs/kit';
-
-export const config: Config = {
-  isr: {
-    expiration: 60 * 60 * 24 // 1 day
-  }
-};
