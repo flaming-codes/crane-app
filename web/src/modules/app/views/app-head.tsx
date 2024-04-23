be11579f41a8145b59b43797dfc3a65fbf011135
Tom Schönmann <tom@wild.as>
2024-04-23 22:41:56 +0200

    feat: add svgx

--- web/src/modules/app/views/app-head.tsx
+++ web/src/modules/app/views/app-head.tsx
@@ -2,19 +2,21 @@ import { component$ } from "@builder.io/qwik";
 import { Link } from "@builder.io/qwik-city";
 import {
   TbBrandGithub,
-  TbHome2,
   TbNews,
   TbSectionSign,
 } from "@qwikest/icons/tablericons";
+import SineLogo from "~/resources/sine-logo.svg?component";
 
 export const AppHead = component$(() => {
   return (
     <>
       <header class="flex items-center justify-between">
-        <nav class="flex items-center gap-4">
-          <Link href="/">
-            <TbHome2 />
-          </Link>
+        <nav class="flex flex-1 items-center gap-4 ">
+          <div class="flex-1">
+            <Link href="/">
+              <SineLogo class="h-2 text-mauve-11" />
+            </Link>
+          </div>
           <Link href="/blog">
             <TbNews />
           </Link>
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig, type UserConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import svgx from "@svgx/vite-plugin-qwik";
 import pkg from "./package.json";
 
 const { dependencies = {}, devDependencies = {} } = pkg as any as {
@@ -19,7 +20,7 @@ const { dependencies = {}, devDependencies = {} } = pkg as any as {
  */
 export default defineConfig(({ command, mode }): UserConfig => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [qwikCity(), qwikVite(), tsconfigPaths(), svgx()],
     // This tells Vite which dependencies to pre-build in dev mode.
     optimizeDeps: {
       // Put problematic deps that break bundling here, mostly those with binaries.
