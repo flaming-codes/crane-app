ce8c4e62f7a9d94864a4aa0c8dedc43c36607027
Tom Schönmann <tom@wild.as>
2024-09-01 21:53:37 +0200

    feat: add initial scss-support

--- web-v2/src/routes/settings/+page.svelte
+++ web-v2/src/routes/settings/+page.svelte
@@ -1,5 +1,8 @@
 <script>
   import {
+    Breadcrumb,
+    BreadcrumbItem,
+    Column,
     Content,
     Grid,
     RadioButton,
@@ -16,14 +19,38 @@
 
 <Content>
   <Grid>
-    <Row><h2>Settings</h2></Row>
+    <Row>
+      <Column>
+        <h1 class="text">Settings</h1>
+
+        <p>This is a settings page. You can change your preferences here.</p>
+      </Column>
+    </Row>
+    <Row>
+      <Column>
+        <Breadcrumb>
+          <BreadcrumbItem href="/">Dashboard</BreadcrumbItem>
+          <BreadcrumbItem href="/settings">Annual reports</BreadcrumbItem>
+        </Breadcrumb>
+      </Column>
+    </Row>
 
     <Row>
-      <RadioButtonGroup legendText="Carbon theme" bind:selected={theme}>
-        {#each ['white', 'g10', 'g80', 'g90', 'g100'] as value}
-          <RadioButton labelText={value} {value} />
-        {/each}
-      </RadioButtonGroup>
+      <Column>
+        <RadioButtonGroup legendText="Carbon theme" bind:selected={theme}>
+          {#each ['white', 'g10', 'g80', 'g90', 'g100'] as value}
+            <RadioButton labelText={value} {value} />
+          {/each}
+        </RadioButtonGroup>
+      </Column>
     </Row>
   </Grid>
 </Content>
+
+<style lang="scss">
+  @use '@carbon/type';
+
+  .text {
+    @include type.type-style('heading-07');
+  }
+</style>
