d5279bd3c3319ab9855255458d52d1c38c93194f
Tom Schönmann <tom@wild.as>
2024-10-23 16:38:13 +0200

    wip

--- app/app/routes/_index.tsx
+++ app/app/routes/_index.tsx
@@ -19,7 +19,7 @@ export default function Index() {
         <span>Search</span>
       </nav>
 
-      <div className="full-width bg-gradient-to-tr from-iris-10  text-gray-1 py-4 min-h-48">
+      <div className="full-width bg-gradient-to-tr from-iris-10 py-4 min-h-48 text-gray-1">
         <div className="flex flex-col md:flex-row gap-2 items-center justify-between">
           <div className=" flex flex-col justify-center gap-1">
             <h1 className="text-4xl font-light">GaussSuppression</h1>
@@ -33,8 +33,8 @@ export default function Index() {
         </div>
       </div>
 
-      <nav className=" full-width overflow-x-auto divide-x divide-gray-12 border-y border-gray-12 text-xs sticky top-14 backdrop-blur-lg">
-        <div className="flex gap-1">
+      <nav className=" full-width overflow-x-auto border-y border-gray-12 text-xs sticky top-14 backdrop-blur-lg">
+        <div className="flex">
           {[
             "Synopsis",
             "Statistics",
@@ -46,7 +46,7 @@ export default function Index() {
             <Link
               key={item}
               to={`#${item.toLowerCase()}`}
-              className="min-w-24 text-center py-3 shrink-0 flex-1 border-b border-transparent hover:border-gray-8 transition-colors"
+              className="min-w-24 text-center py-3 shrink-0 flex-1 border-b border-transparent hover:border-gray-normal transition-colors"
             >
               {item}
             </Link>
@@ -56,7 +56,7 @@ export default function Index() {
 
       <div className="full-width pt-16">
         <div className="flex flex-col gap-16">
-          <section className="border-b border-gray-11 pb-8">
+          <section className="border-b border-gray-normal pb-8">
             <blockquote className="text-xl font-light leading-normal">
               A statistical disclosure control tool to protect tables by
               suppression using the Gaussian elimination secondary suppression
