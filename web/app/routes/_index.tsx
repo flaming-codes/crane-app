e724c5d6a0cab4fc7cc3931469ebcde76799da66
Tom Schönmann <tom@wild.as>
2024-10-27 21:41:21 +0100

    chore: update gradient animation

--- web/app/routes/_index.tsx
+++ web/app/routes/_index.tsx
@@ -76,38 +76,45 @@ export default function Index() {
   );
 }
 
-function GradientBackground({ meshIndex }: { meshIndex: number }) {
+function GradientBackground({ meshIndex }: { meshIndex: null | number }) {
+  if (meshIndex === null) {
+    return null;
+  }
   return (
     <>
       <div
-        className={clsx("fixed inset-x-0 top-0 -z-50 h-[60vh] animate-fade", {
-          "bg-mesh-1": meshIndex === 0,
-          "bg-mesh-2": meshIndex === 1,
-          "bg-mesh-3": meshIndex === 2,
-          "bg-mesh-4": meshIndex === 3,
-          "bg-mesh-5": meshIndex === 4,
-          "bg-mesh-6": meshIndex === 5,
-          "bg-mesh-7": meshIndex === 6,
-          "bg-mesh-8": meshIndex === 7,
-          "bg-mesh-9": meshIndex === 8,
-          "bg-mesh-10": meshIndex === 9,
-          "bg-mesh-11": meshIndex === 10,
-          "bg-mesh-12": meshIndex === 11,
-          "bg-mesh-13": meshIndex === 12,
-          "bg-mesh-14": meshIndex === 13,
-          "bg-mesh-15": meshIndex === 14,
-          "bg-mesh-16": meshIndex === 15,
-          "bg-mesh-17": meshIndex === 16,
-          "bg-mesh-18": meshIndex === 17,
-          "bg-mesh-19": meshIndex === 18,
-          "bg-mesh-20": meshIndex === 19,
-          "bg-mesh-21": meshIndex === 20,
-          "bg-mesh-22": meshIndex === 21,
-          "bg-mesh-23": meshIndex === 22,
-          "bg-mesh-24": meshIndex === 23,
-          "bg-mesh-25": meshIndex === 24,
-        })}
+        className={clsx(
+          "fixed inset-x-0 top-0 -z-50 h-[60vh] transform-gpu animate-fade animate-duration-500",
+          {
+            "bg-mesh-1": meshIndex === 0,
+            "bg-mesh-2": meshIndex === 1,
+            "bg-mesh-3": meshIndex === 2,
+            "bg-mesh-4": meshIndex === 3,
+            "bg-mesh-5": meshIndex === 4,
+            "bg-mesh-6": meshIndex === 5,
+            "bg-mesh-7": meshIndex === 6,
+            "bg-mesh-8": meshIndex === 7,
+            "bg-mesh-9": meshIndex === 8,
+            "bg-mesh-10": meshIndex === 9,
+            "bg-mesh-11": meshIndex === 10,
+            "bg-mesh-12": meshIndex === 11,
+            "bg-mesh-13": meshIndex === 12,
+            "bg-mesh-14": meshIndex === 13,
+            "bg-mesh-15": meshIndex === 14,
+            "bg-mesh-16": meshIndex === 15,
+            "bg-mesh-17": meshIndex === 16,
+            "bg-mesh-18": meshIndex === 17,
+            "bg-mesh-19": meshIndex === 18,
+            "bg-mesh-20": meshIndex === 19,
+            "bg-mesh-21": meshIndex === 20,
+            "bg-mesh-22": meshIndex === 21,
+            "bg-mesh-23": meshIndex === 22,
+            "bg-mesh-24": meshIndex === 23,
+            "bg-mesh-25": meshIndex === 24,
+          },
+        )}
       />
+
       <div className="fixed inset-x-0 top-0 -z-50 h-[60vh] bg-gradient-to-t from-white dark:from-black" />
       <div className="fixed inset-x-0 top-0 -z-40 h-[60vh] bg-gradient-to-t from-white via-white opacity-50 dark:from-black dark:via-black" />
       <div className="fixed inset-x-0 top-[40vh] -z-30 h-[20vh] bg-gradient-to-t from-white via-white opacity-80 dark:from-black dark:via-black" />
