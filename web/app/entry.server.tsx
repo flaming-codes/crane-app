af165896c28f30d03f85007cce4c0c3fc7718d9a
Tom Schönmann <11635736+flaming-codes@users.noreply.github.com>
2024-12-23 23:53:36 +0100

    feat: add otel traces (#208)

--- web/app/entry.server.tsx
+++ web/app/entry.server.tsx
@@ -13,6 +13,9 @@ import { renderToPipeableStream } from "react-dom/server";
 import { server } from "./mocks/node.server";
 import { slog } from "./modules/observability.server";
 import { createSecureHeaders } from "@mcansh/http-helmet";
+// Import the instrumentation server module,
+// which will initialize the OpenTelemetry SDK.
+import "./modules/instrumentation.server";
 
 const ABORT_DELAY = 5_000;
 
