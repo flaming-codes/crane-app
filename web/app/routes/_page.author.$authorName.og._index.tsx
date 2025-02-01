00dc6d8e9ba2d84e36b3fba8c281fa20c0c24655
Tom Schönmann <tom@flaming.codes>
2025-02-01 21:01:35 +0100

    chore: remove comments

--- web/app/routes/_page.author.$authorName.og._index.tsx
+++ web/app/routes/_page.author.$authorName.og._index.tsx
@@ -21,13 +21,10 @@ export const loader = async ({ request, params }: LoaderFunctionArgs) => {
     requestUrl: origin,
   });
 
-  // Respond with the PNG buffer
   return new Response(png, {
     status: 200,
     headers: {
-      // Tell the browser the response is an image
       "Content-Type": "image/png",
-      // Tip: You might want to heavily cache the response in production
       "cache-control":
         ENV.NODE_ENV === "production"
           ? `public, immutable, no-transform, max-age=${hoursToSeconds(24 * 7)}`
--- web/app/routes/_page.press.$articleType.og._index.tsx
+++ web/app/routes/_page.press.$articleType.og._index.tsx
@@ -12,13 +12,10 @@ export const loader = async ({ request, params }: LoaderFunctionArgs) => {
     articleType: params.articleType as string,
   });
 
-  // Respond with the PNG buffer
   return new Response(png, {
     status: 200,
     headers: {
-      // Tell the browser the response is an image
       "Content-Type": "image/png",
-      // Tip: You might want to heavily cache the response in production
       "cache-control":
         ENV.NODE_ENV === "production"
           ? `public, immutable, no-transform, max-age=${hoursToSeconds(24 * 7)}`
