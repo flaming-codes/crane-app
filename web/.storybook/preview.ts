0978dcad7a2902f1c1687dc3624249c07f4e503c
Tom Schönmann <tom@wild.as>
2025-12-22 00:09:00 +0100

    feat: Refactor Storybook MDX documentation to use `@storybook/addon-docs/blocks` and simpler Markdown, update dependencies, and add autodocs tag.

--- web/.storybook/preview.ts
+++ web/.storybook/preview.ts
@@ -9,7 +9,7 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
-
+    tags: ["autodocs"],
     a11y: {
       // 'todo' - show a11y violations in the test UI only
       // 'error' - fail CI on a11y violations
